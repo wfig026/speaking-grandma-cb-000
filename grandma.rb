02bd685478a3a2e124d854416ffdecf5de875152
Will Figueiredo <will@wfigueiredo.com>
2018-09-07 00:47:45 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,9 +3,9 @@ def speak_to_grandma(string)
   if string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif string.upcase
-    return "I LOVE YOU TOO PUMPKIN!"
-  else string.upcase
     return "NO, NOT SINCE 1938!"
+  else string.upcase
+    return "I LOVE YOU TOO PUMPKIN!"
   end
 end
 
