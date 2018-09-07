80f2f0d22d90d08138061d37b867a28537112f32
Will Figueiredo <will@wfigueiredo.com>
2018-09-07 00:44:28 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,11 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma()
-
-end
+def speak_to_grandma(string)
+  if string != string.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif string.upcase
+    return "NO, NOT SINCE 1938!"
+  end
+end 
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
